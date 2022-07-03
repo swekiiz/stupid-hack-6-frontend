91889cffe1f485207ad9c6d7026bcdbe4a20869f
Thana <kookza01@gmail.com>
2022-07-03 10:25:11 +0700

    ทำเสร็จได้ไงวะ

--- src/components/feed/NeWPoXt.tsx
+++ src/components/feed/NeWPoXt.tsx
@@ -24,8 +24,16 @@ export const NeWPoXt = () => {
     p: 4,
   }
 
+  const [command, setCommand] = useState<string>(`
+	const mongoose = require('mongoose');
+	try {const postSchema = new mongoose.Schema({image: String, owner: String, comment: String}, {timestamps: true}); mongoose.model('posts', postSchema);} catch(error) {}
+	const Post = mongoose.model('posts');
+	(async() => {try {const post = new Post({image: '#image', owner: 'hacker', comment: '#comment'}); await post.save(); res.send(post);} catch(error) {res.status(200).send({error: error.message})}})()
+	`)
+
   const [open, setOpen] = useState(false)
   const [orgImage, setOrgImage] = useState('')
+  const [orgFile, setOrgFile] = useState('')
   const [editedImage, setEditedImage] = useState('')
   const [faces, setFaces] = useState<
     {
@@ -38,27 +46,38 @@ export const NeWPoXt = () => {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
   const [selectedFace, setSelectedFace] = useState(-1)
+  const [selectedTemplate, setSelectedTemplate] = useState(-1)
+  const [loading, setLoading] = useState(false)
+  const [comment, setComment] = useState('')
 
   const handleOnChange = async (e) => {
+    if (loading) return
+    setLoading(true)
     if (e.target.files && e.target.files[0]) {
       setOrgImage(URL.createObjectURL(e.target.files[0]))
       const file = e.target.files[0]
+      setOrgFile(file)
       const formData = new FormData()
-      formData.append('faceSelected', selectedFace.toString())
-      formData.append('template', '0')
+
+      if (editedImage) {
+        formData.append('faceSelected', selectedFace.toString())
+        formData.append('template', selectedTemplate.toString())
+      }
       formData.append('image', file)
 
       try {
-        const res = await axios('http://localhost:3333/api/preprocess', {
+        const res = await axios('http://103.169.67.41:3333/api/preprocess', {
           method: 'POST',
           data: formData,
           headers: { 'Content-Type': 'multipart/form-data' },
         })
+
         if (res.data?.image) {
           const bytestring = res.data.image
           const image = bytestring.split("'")[1]
           setEditedImage('data:image/jpeg;base64,' + image || orgImage)
         }
+
         if (res.data?.faces.length) {
           const imgSize = res.data?.size
           const { h, w } = imgSize
@@ -75,8 +94,8 @@ export const NeWPoXt = () => {
       } catch (e) {
         console.log(e)
       }
+      setLoading(false)
     }
-
     // fetch(`${API_URL}/image-upload`, {
     //   method: 'POST',
     //   body: formData,
@@ -88,6 +107,75 @@ export const NeWPoXt = () => {
     //       images,
     //     })
     //   })
+  }
+
+  const handlerApply = async () => {
+    if (loading) return
+
+    setLoading(true)
+    const formData = new FormData()
+    const file = orgFile
+
+    if (editedImage) {
+      formData.append('faceSelected', selectedFace.toString())
+      formData.append('template', selectedTemplate.toString())
+    }
+    formData.append('image', file)
+
+    try {
+      const res = await axios('http://103.169.67.41:3333/api/preprocess', {
+        method: 'POST',
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+
+      if (res.data?.image) {
+        const bytestring = res.data.image
+        const image = bytestring.split("'")[1]
+        setEditedImage('data:image/jpeg;base64,' + image || orgImage)
+      }
+
+      if (res.data?.faces.length) {
+        const imgSize = res.data?.size
+        const { h, w } = imgSize
+        // drawRect({ x: 1997, y: 20, w: 50, h: 60 })
+        setFaces(
+          res.data?.faces.map((e) => {
+            return { x: (e[0] / w) * 800, y: (e[1] / h) * 600, w: (e[2] / w) * 800, h: (e[3] / h) * 600 }
+            // drawRect()
+          }),
+        )
+
+        console.log('เย่')
+      }
+    } catch (e) {
+      console.log(e)
+    }
+
+    setLoading(false)
+  }
+
+  const handlerSave = async () => {
+    if (loading) return
+    setLoading(true)
+
+    try {
+      const res = await axios(config.ENDPOINT || '', {
+        method: 'POST',
+        data: {
+          command: command.replace('#comment', comment).replace('#image', editedImage),
+        },
+        headers: {
+          X_API_KEY: config.X_API_KEY || '',
+        },
+      })
+
+      console.log(res.data)
+    } catch (e) {
+      console.log(e)
+    }
+
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -116,7 +204,14 @@ export const NeWPoXt = () => {
     // drawRect(r1Info)
   }, [])
 
-  const templates = ['hack1', 'hack2', 'hack3']
+  const templates = [
+    'replaceAllFace',
+    'shuffleAllFace',
+    'replaceAllFaceWithOriginalPhoto',
+    'replaceAllFaceWithSelectedPhoto',
+    'blurImage',
+    'blurAllFace',
+  ]
 
   return (
     <Box style={{ display: 'flex', padding: '20px', justifyContent: 'center' }}>
@@ -156,17 +251,18 @@ export const NeWPoXt = () => {
                         position: 'absolute',
                         left: `${e.x}px`,
                         top: `${e.y}px`,
-                        width: `${e.w} px`,
-                        height: `${e.h} px`,
-                        backgroundColor: 'red',
+                        width: `${e.w}px`,
+                        height: `${e.h}px`,
+                        border: '2px solid pink',
+                        // backgroundColor: 'red',
+                        display: 'inline-block',
+                        cursor: 'pointer',
                       }}
                       key={idx}
                       onClick={() => {
                         setSelectedFace(idx)
                       }}
-                    >
-                      click
-                    </div>
+                    />
                   ))}
               </div>
               <div>
@@ -176,13 +272,42 @@ export const NeWPoXt = () => {
             <Box style={{ width: '100%', padding: '8px', display: 'flex', flexDirection: 'column' }}>
               <FormControl>
                 <FormLabel id="template">Template</FormLabel>
-                <RadioGroup row aria-labelledby="template" name="template">
+                <RadioGroup
+                  row
+                  aria-labelledby="template"
+                  name="template"
+                  onChange={(e) => setSelectedTemplate(parseInt(e.target.value))}
+                >
                   {templates.map((e, idx) => (
                     <FormControlLabel value={idx} control={<Radio />} label={e} key={idx} />
                   ))}
                 </RadioGroup>
               </FormControl>
-              <Button>Apply</Button>
+              <p>
+                Face Selected: <span>{selectedFace.toString()}</span>
+              </p>
+              <Button
+                onClick={() => {
+                  handlerApply()
+                }}
+              >
+                Apply
+              </Button>
+              <p>Comment</p>
+              <textarea
+                placeholder="."
+                value={comment}
+                onChange={(e) => {
+                  setComment(e.target.value)
+                }}
+              />
+              <Button
+                onClick={() => {
+                  handlerSave()
+                }}
+              >
+                Save
+              </Button>
             </Box>
           </Typography>
         </Box>
